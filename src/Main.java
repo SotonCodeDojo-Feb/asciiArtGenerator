b59fe2b47e5599c21310c9db9bea3cb18f95900e
Huw Jones <huwcbjones@gmail.com>
2016-02-11 20:00:08 +0000

    Updated main method

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.io.*;
 /**
  * {DESCRIPTION}
  *
@@ -7,6 +8,18 @@
 public class Main {
 
     public static void main(String args[]){
+        if(args.length != 1){
+            System.err.println("No picture specified.");
+            return;
+        }
+        File image = new File(args[0]);
+        if(!image.exists()){
+            System.err.println("Image \"" + args[0] + "\" not found!");
+        }
+        if(!image.canRead()){
+            System.err.println("Image \"" + args[0] + "\" could not be opened!");
+        }
 
+        asciiGenerator generator = new asciiGenerator(args[0]);
     }
 }
