970b6c12ad568e0b5e57cf993ffeab01e525460c
Huw Jones <huwcbjones@gmail.com>
2016-02-11 20:04:39 +0000

    Added method stubs

--- src/asciiGenerator.java
+++ src/asciiGenerator.java
@@ -1,3 +1,5 @@
+import java.awt.*;
+
 /**
  * ASCII Art Generator
  *
@@ -6,7 +8,23 @@
  */
 public class asciiGenerator {
 
-    public asciiGenerator(String picture){
+    public asciiGenerator (String picture) {
+
+    }
+
+    private Image loadImage (String image) {
+
+    }
+
+    private Image resizeImage (Image image) {
+
+    }
+
+    private Image convertToGreyScale (Image image) {
+
+    }
+
+    private String getASCII (Image image){
 
     }
 }
