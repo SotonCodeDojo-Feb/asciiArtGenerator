fcd447193d3793c18330a0d1fc7127e8608799e9
Huw Jones <huwcbjones@gmail.com>
2016-02-11 20:33:06 +0000

    Finished getASCII method

--- src/asciiGenerator.java
+++ src/asciiGenerator.java
@@ -2,6 +2,7 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.util.HashMap;
+
 import org.json.*;
 
 /**
@@ -15,18 +16,17 @@ public class asciiGenerator {
     private HashMap<Integer, Character> charMap;
 
     public asciiGenerator (String picture) {
-        if(!loadCharMap()){
+        if (!loadCharMap()) {
             System.err.println("Failed to load character map.\n Quitting...");
             return;
         }
     }
 
-    private boolean loadCharMap(){
+    private boolean loadCharMap () {
         File map = new File("map.json");
-        if(!map.exists() || !map.canRead()){
+        if (!map.exists() || !map.canRead()) {
             return false;
         }
-
     }
 
     //
@@ -44,12 +44,12 @@ public class asciiGenerator {
         for (int x = 0; x < image.getWidth(); ++x)
             for (int y = 0; y < image.getHeight(); ++y) {
                 int rgb = image.getRGB(x, y);
-                int r = (rgb >> 16) & 0xFF;
-                int g = (rgb >> 8) & 0xFF;
-                int b = (rgb & 0xFF);
+                int r = ( rgb >> 16 ) & 0xFF;
+                int g = ( rgb >> 8 ) & 0xFF;
+                int b = ( rgb & 0xFF );
 
-                int grayLevel = (r + g + b) / 3;
-                int gray = (grayLevel << 16) + (grayLevel << 8) + grayLevel;
+                int grayLevel = ( r + g + b ) / 3;
+                int gray = ( grayLevel << 16 ) + ( grayLevel << 8 ) + grayLevel;
                 image.setRGB(x, y, gray);
             }
         return image;
@@ -57,16 +57,18 @@ public class asciiGenerator {
 
     // Huw's Job
     private String getASCII (BufferedImage image) {
+        String picture = "";
 
-        for (int x = 0; x < image.getWidth(); x++) {
-            for (int y = 0; y < image.getHeight(); y++) {
-                float luminosity = (image.getRGB(x, y) & 0xff) / 16f;
+        for (int y = 0; y < image.getHeight(); y++) {
+            for (int x = 0; x < image.getWidth(); x++) {
+                float luminosity = ( image.getRGB(x, y) & 0xff ) / 16f;
                 int charValue = Double.valueOf(Math.floor(luminosity)).intValue();
 
-
+                picture += this.charMap.get(charValue);
             }
+            picture += "\n";
         }
 
-
+        return picture;
     }
 }
