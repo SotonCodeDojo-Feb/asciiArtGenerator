d8ae286ae6eddc90071f1ac7da0c46d9755901fc
Huw Jones <huwcbjones@gmail.com>
2016-02-11 19:56:12 +0000

    Added constructor for generator

--- src/asciiGenerator.java
+++ src/asciiGenerator.java
@@ -1,8 +1,12 @@
 /**
- * {DESCRIPTION}
+ * ASCII Art Generator
  *
  * @author Huw Jones
  * @since 11/02/2016
  */
 public class asciiGenerator {
+
+    public asciiGenerator(String picture){
+
+    }
 }
