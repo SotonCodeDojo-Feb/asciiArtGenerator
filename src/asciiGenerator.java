09b6fcbc194eb2208d00ec35de60e2f464f8f3a8
Jamie Scott <github@jami.org.uk>
2016-02-11 20:20:17 +0000

    added constructor code and convert to greyscale

--- src/asciiGenerator.java
+++ src/asciiGenerator.java
@@ -9,12 +9,17 @@ import java.awt.image.BufferedImage;
  */
 public class asciiGenerator {
 
-    public asciiGenerator (String picture) {
-
+    public asciiGenerator (String imageFile) {
+        BufferedImage image;
+        image = this.loadImage(imageFile);
+        image = this.resizeImage(image);
+        image = this.convertToGreyScale(image);
+        String output = this.getASCII(image);
+        System.out.println(output);
     }
 
     //
-    private BufferedImage loadImage (BufferedImage image) {
+    private BufferedImage loadImage (String imageFile) {
 
     }
 
@@ -25,7 +30,18 @@ public class asciiGenerator {
 
     // Jamie's Task
     private BufferedImage convertToGreyScale (BufferedImage image) {
+        for (int x = 0; x < image.getWidth(); ++x)
+            for (int y = 0; y < image.getHeight(); ++y) {
+                int rgb = image.getRGB(x, y);
+                int r = (rgb >> 16) & 0xFF;
+                int g = (rgb >> 8) & 0xFF;
+                int b = (rgb & 0xFF);
 
+                int grayLevel = (r + g + b) / 3;
+                int gray = (grayLevel << 16) + (grayLevel << 8) + grayLevel;
+                image.setRGB(x, y, gray);
+            }
+        return image;
     }
 
     // Huw's Job
